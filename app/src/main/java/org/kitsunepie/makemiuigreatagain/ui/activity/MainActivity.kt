b8229d94725ad74eb9c5629c1cba7ab9d61bd921
KyuubiRan <KyuubiRan@qq.com>
2021-11-13 23:03:22 +0800

    添加 基础UI

---
+++ app/src/main/java/org/kitsunepie/makemiuigreatagain/ui/activity/MainActivity.kt
@@ -0,0 +1,111 @@
+package org.kitsunepie.makemiuigreatagain.ui.activity
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Switch
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.github.kyuubiran.ezxhelper.init.EzXHelperInit
+import org.kitsunepie.makemiuigreatagain.ui.theme.Blue200
+import org.kitsunepie.makemiuigreatagain.ui.theme.WhiteTheme
+
+class MainActivity : ComponentActivity() {
+
+    @Composable
+    fun MainUI() {
+        CardGroup("图库") {
+            SwitchItem("无限裁剪", "移除截图/图片裁剪最小限制")
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        EzXHelperInit.initAppContext(applicationContext)
+        setContent {
+            WhiteTheme {
+                MainUI()
+            }
+        }
+    }
+
+    @Composable
+    fun SwitchItem(
+        title: String,
+        desc: String? = null,
+        checked: MutableState<Boolean> = remember {
+            mutableStateOf(false)
+        },
+        onChange: ((Boolean) -> Unit)? = null,
+        onClick: (() -> Unit)? = null,
+    ) {
+        Box(modifier = Modifier
+            .fillMaxWidth()
+            .clickable {
+                if (onClick == null) {
+                    checked.value = !checked.value
+                } else {
+                    onClick()
+                }
+            }) {
+            Row {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth(0.85f)
+                        .height(50.dp)
+                        .background(Color.White, RoundedCornerShape(13.dp))
+                        .padding(horizontal = 15.dp),
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = title,
+                        color = Color.Black,
+                        fontSize = 18.sp
+                    )
+                    desc?.let {
+                        Text(
+                            text = desc,
+                            fontSize = 14.sp,
+                            color = Color.Gray
+                        )
+                    }
+                }
+                Switch(
+                    checked = checked.value,
+                    onCheckedChange = {
+                        checked.value = it
+                        onChange?.invoke(it)
+                    },
+                )
+            }
+        }
+
+    }
+
+    @Composable
+    fun CardGroup(title: String, content: @Composable () -> Unit) {
+        Card(modifier = Modifier.padding(10.dp)) {
+            Column {
+                Text(
+                    modifier = Modifier.padding(start = 10.dp, top = 10.dp),
+                    text = title,
+                    color = Blue200,
+                    fontSize = 20.sp
+                )
+                content()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/org/kitsunepie/makemiuigreatagain/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package org.kitsunepie.makemiuigreatagain.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Pink200 = Color(0xFFFF86FC)
+val Pink500 = Color(0xFFFF00EE)
+val Pink700 = Color(0xFFFF00B3)
+val Teal200 = Color(0xFF03DAC5)
+val LightBlue = Color(0xFFD8E6FF)
+val Blue = Color(0xFF76A9FF)
+val Blue200 = Color(0xFF2186FC)
